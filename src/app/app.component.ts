33d0b3799afb60674a7dbc8adc80afbf41668465
alu0100836059 <alu0100836059@ull.edu.es>
2017-10-25 14:25:34 +0200

    Added a simple animation to a paragraph element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      state('smaller', style({
+        transform: 'scale(0)',
+        color: 'blue'
+      })),
+      state('larger', style({
+        transform: 'scale(3)',
+        color: 'red'
+      })),
+      transition('smaller <=> larger', animate('450ms ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+
+  state: string = 'smaller';
+  animate() {
+    this.state = this.state == 'larger' ? 'smaller' : 'larger';
+  }
 }
